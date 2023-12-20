54f6bbcc9d71f7e39110d00bbb3e2fc1bf0eb811
Ammar Khan <ammarkofficial@gmail.com>
2023-12-20 20:34:09 +0500

    Day 2 Added

---
+++ Day_2/Edverterr/src/App.jsx
@@ -0,0 +1,22 @@
+
+// Hook is used to store something
+// Insted of class use className
+
+import About from "./About"
+import Footer from "./Footer"
+import Header from "./Header"
+
+
+const App = () => {
+  return (
+    <>
+     <Header />
+     <About />
+     <Footer />
+
+    </>
+    
+  )
+}
+
+export default App
---
+++ Day_2/Edverterr/src/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+        <h2>Created By Ammar</h2>
+        <h2>Valar Mogolis</h2>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
